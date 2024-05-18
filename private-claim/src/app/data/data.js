72668be3040d421b92be0029896264ae1a873396
Bigjoe.js <98628310+bigjoefilms@users.noreply.github.com>
2024-05-18 17:10:24 +0100

    first commit

---
+++ private-claim/src/app/data/data.js
@@ -0,0 +1,659 @@
+const batch1 = [
+    "oluwafemialofe@yahoo.com",
+    "oluwafemialofe@gmail.com",
+    "alexjoel001@proton.me",
+    "ebensohenry@gmail.com",
+    "smillermurmo@gmail.com",
+    "drojiugo@gmail.com",
+    "airdropm561@gmail.com",
+    "chidialaka@yahoo.com",
+    "zeedalee1@gmail.com",
+    "brightifeyi05@gmail.com",
+    "silas.bethel10@gmail.com",
+    "txchill@outlook.com",
+    "wavecrestfx@gmail.com",
+    "nnajiebuka81@gmail.com",
+    "meetdbomb@yahoo.com",
+    "idrisfolar@gmail.com",
+    "dzone45ng@yahoo.com",
+    "tolulopefelix21@gmail.com",
+    "ayobamiemmanuel13@gmail.com",
+    "saidatabiola2@gmail.com",
+    "Maleek12adeleke@gmail.com",
+    "livinusuchenna4@gmail.com",
+    "olufunkeadeyemo3@gmail.com",
+    "damolaolayinka261@gmail.com",
+    "abdulrasaq201@gmail.com",
+    "joysmart2061@gmail.com",
+    "chijiokennabude@gmail.com",
+    "Ajikanleola70@gmail.com",
+    "Nkasiobihenry@gmail.com",
+    "essienjoseph62@gmail.com",
+    "olusolataiwo14@gmail.com",
+    "emmanuelonotiemoria@gmail.com",
+    "idowuileriayo@gmail.com",
+    "ositadimma63@gmail.com",
+    "newuniversalbitz@gmail.com",
+    "solomonadejere553@gmail.com",
+    "vincentaluka1@gmail.com",
+    "Coinchika7@gmail.com",
+    "jayquasim@gmail.com",
+    "emailjoe38@gmail.com",
+    "webthreeking@gmail.com",
+    "samosky185@gmail.com",
+    "adowori@gmail.com",
+    "amaewhulem@gmail.com",
+    "philipgodschosen@gmail.com",
+    "ebenezeralozie2016@gmail.com",
+    "chinedudibua@gmail.com",
+    "ugochuwugospel@gmail.com",
+    "royphilips101@gmail.com",
+    "namba8park99@gmail.com",
+    "ogechialex1512019@gmail.com",
+    "dimmamercyo@gmail.com",
+    "mail2onimisi@gmail.com",
+    "opetmedlab@gmail.com",
+    "omohdominic@gmail.com",
+    "joseph.blessing299@yahoo.com",
+    "chiemekacharles@gmail.com",
+    "qumuqethu47@gmail.com",
+    "collinsb335@gmail.com",
+    "akpedefestus@gmail.com",
+    "xcryptonaire@gmail.com",
+    "theadabillions@gmail.com",
+    "josephjoey289@gmail.com",
+    "omachefujonah@yahoo.com",
+    "chuko1779@gmail.com",
+    "nwachukwucollins324@gmail.com",
+    "conquerbillions@gmail.com",
+    "mpozam79@gmail.com",
+    "skyllink@yahoo.com",
+    "onyedikafrank1@gmail.com",
+    "talabitope7@gmail.com",
+    "osinachiobi95@gmail.com",
+    "lovethizekor123@gmail.com",
+    "supet3017@gmail.com",
+    "samologunja2@gmail.com",
+    "2023ola4life@gmail.com",
+    "abdul.tawfik25@gmail.com",
+    "udavid802@gmail.com",
+    "opeyeminelson2@gmail.com",
+    "ugokelvinoffor962@gmail.com",
+    "ndiokweluifeanyi64@gmail.com",
+    "abiodunmunna@gmail.com",
+    "jjjjrock9999@gmail.com",
+    "omoboyefemibamidele@gmail.com",
+    "salma.kimisha@gmail.com",
+    "umohp63@gmail.com",
+    "Emeriejchuks@gmail.com",
+    "andrewochigbu@gmail.com",
+    "Testeremail219@gmail.com",
+    "cryptoboomers7@gmail.com",
+    "sundaynwodo80s@gmail.com",
+    "squallemilz0@gmail.com",
+    "udikechisom@gmail.com",
+    "robinsonokoye2@gmail.com",
+    "kmartmicheals@gmail.com",
+    "festusigbolanu@gmail.com",
+    "greatworldng@gmail.com",
+    "bogdaniosifescu@yahoo.com",
+    "udekwuchukwuebuka@gmail.com",
+    "chukwuglory606@gmail.com",
+    "kufrejimmy32@gmail.com",
+  "ephraimodinaka22@gmail.com",
+  "shittusul20@gmail.com",
+  "agborzema7@gmail.com",
+  "fombangnelson908@gmail.com",
+  "gracefuambeh@gmail.com",
+  "adeyemi8160@gmail.com",
+  "eloskidyace@gmail.com",
+  "Timiadekunle2794@gmail.com",
+  "etwetebb@yahoo.com",
+  "janetoyelade@gmail.com",
+  "iwunzechidimma@gmail.com",
+  "iamprincechukwu@gmail.com",
+  "Kennedyalaka8@gmail.com",
+  "laryll4sure@gmail.com",
+  "bomageorge28@gmail.com",
+  "igbonanbuchi@gmail.com",
+  "seniosibanjo27@gmail.com",
+  "ajvictooor@gmail.com",
+  "mathydawu@gmail.com",
+  "akaron404@gmail.com",
+  "amezhinimwalele@gmail.com",
+  "amosoluwakorede@gmail.com",
+  "betterways247@gmail.com",
+  "temmysanusii@gmail.com",
+  "getapay@gmail.com",
+  "olatundejoshua93@gmail.com",
+  "amaobiisaiah@gmail.com",
+  "akum.njeri@gmail.com",
+  "agbohaprecious@gmail.com",
+  "brymefx@gmail.com",
+  "mmaeyen4christ2018@gmail.com",
+  "anjanbarai@gmail.com",
+  "billiom354@gmail.com",
+  "okonkwochiedoziejoseph@gmail.com",
+  "ukorahprince207@gmail.com",
+  "emekaibemene@gmail.com",
+  "odiraogbu@gmail.com",
+  "abdulwasiu.alli@gmail.com",
+  "olagunju6108@gmail.com",
+  "emmanuelik15@gmail.com",
+  "goodymomah8@gmail.com",
+  "sadeniyi009@gmail.com",
+  "apologunroy@gmail.com",
+  "hannahotuoba1984@gmail.com",
+  "uchendurosemary01@gmail.com",
+  "adenijigbenga9@gmail.com",
+  "joshuaapekey17@gmail.com",
+
+
+];
+export const batch2 = [
+
+"profdike58@gmail.com",
+"profdike58@gmail.com",
+"a.alofe@gmail.com",
+"luckyokobu890@gmail.com",
+"chimaobijohnbosco9@gmail.com",
+"chiolojisteve@gmail.com",
+"exorux@yahoo.com",
+"victorobika155@gmail.com",
+"divinodoemena@gmail.com",
+"izupays@gmail.com",
+"mimsnbills@gmail.com",
+"only1hecon@gmail.com",
+"zharuna007@gmail.com",
+"daudaadekunle288@gmail.com",
+"henryezeakunne@gmail.com",
+"smesocial52021@gmail.com",
+"mikesthaconcept@gmail.com",
+"favouroko80@gmail.com",
+"sunsaro280@yahoo.com",
+"okorieaugustine057@gmail.com",
+"Brightibok1@gmail.com",
+"fortunateman11@gmail.com",
+"atumyom@gmail.com",
+"indussoftcom7@gmail.com",
+"leximio85@gmail.com",
+"queensleyjael@gmail.com",
+"johnbosco1313@gmail.com",
+"grakins21@yahoo.com",
+"bewamu2@gmail.com",
+"princelintikeng@gmail.com",
+"adutimothyk@gmail.com",
+"rariprajna@gmail.com",
+"checheco941@gmail.com",
+"okekepatrick423@gmail.com",
+"Humane044@gmail.com",
+"Goodcarepharmacy@yahoo.com",
+"bethreal20@gmail.com",
+"jahangirbutt106@gmail.com",
+"islaogolo@gmail.com",
+"kwinpaul24@gmail.com",
+"akandatunde@gmail.com",
+"owhemuohwofasa@gmail.com",
+"fizzymidasquill@gmail.com",
+"diamondelle.co@gmail.com",
+"ifeakorobiageli@gmail.com",
+"3whitedovee@gmail.com",
+"ipv4topaz@gmail.com",
+"oraclewoods75@gmail.com",
+"sirwily4@gmail.com",
+"tristanpatrick51@gmail.com",
+"kennedyiwariso@yahoo.com",
+"johnonso81@gmail.com",
+"falkrum@mail.ru",
+"bshittu23@gmail.com",
+"starstuddedbest@gmail.com",
+"templejr901@gmail.com",
+"princeenure@gmail.com",
+"apostleword96@gmail.com",
+"talktofaith21@gmail.com",
+"brightsneh32@gmail.com",
+"mecusg1@gmail.com",
+"obiloremmanuel03@gmail.com",
+"danoluwaseun18@gmail.com",
+"peaceolamide97@gmail.com",
+"reecehall098@gmail.com",
+"akanrabalu@gmail.com",
+"indussoftcom7@gmail",
+"Mrtimileyinafolabi@gmail.com",
+"sandrahuggins24@gmail.com",
+"may.aregbe@gmail.com",
+"earncryptoz23@gmail.com",
+"donald1stempire@gmail.com",
+"paulesky28@gmail.com",
+"Viv_tama@yahoo.com",
+"profdike58@gmail.com",
+"kelechimoses2023@gmail.com",
+"michellemhlanga56@yahoo.co.za",
+"newworldolawale79@gmail.comn",
+"ebenstallion@gmail.com",
+
+];
+
+// batch1
+// oluwafemialofe@yahoo.com
+// oluwafemialofe@gmail.com
+// alexjoel001@proton.me
+// ebensohenry@gmail.com
+// smillermurmo@gmail.com
+// drojiugo@gmail.com
+// airdropm561@gmail.com
+// chidialaka@yahoo.com
+// zeedalee1@gmail.com
+// brightifeyi05@gmail.com
+// silas.bethel10@gmail.com
+// txchill@outlook.com
+// wavecrestfx@gmail.com
+// nnajiebuka81@gmail.com
+// meetdbomb@yahoo.com
+// idrisfolar@gmail.com
+// dzone45ng@yahoo.com
+// tolulopefelix21@gmail.com
+// ayobamiemmanuel13@gmail.com
+// saidatabiola2@gmail.com
+// Maleek12adeleke@gmail.com
+// livinusuchenna4@gmail.com
+// olufunkeadeyemo3@gmail.com
+// damolaolayinka261@gmail.com
+// abdulrasaq201@gmail.com
+// joysmart2061@gmail.com
+// chijiokennabude@gmail.com
+// Ajikanleola70@gmail.com
+// Nkasiobihenry@gmail.com
+// essienjoseph62@gmail.com
+// olusolataiwo14@gmail.com
+// emmanuelonotiemoria@gmail.com
+// idowuileriayo@gmail.com
+// ositadimma63@gmail.com
+// newuniversalbitz@gmail.com
+// solomonadejere553@gmail.com
+// vincentaluka1@gmail.com
+// Coinchika7@gmail.com
+// jayquasim@gmail.com
+// emailjoe38@gmail.com
+// webthreeking@gmail.com
+// samosky185@gmail.com
+// adowori@gmail.com
+// amaewhulem@gmail.com
+// philipgodschosen@gmail.com
+// ebenezeralozie2016@gmail.com
+// chinedudibua@gmail.com
+// ugochuwugospel@gmail.com
+// royphilips101@gmail.com
+// namba8park99@gmail.com
+// ogechialex1512019@gmail.com
+// dimmamercyo@gmail.com
+// mail2onimisi@gmail.com
+// opetmedlab@gmail.com
+// omohdominic@gmail.com
+// joseph.blessing299@yahoo.com
+// chiemekacharles@gmail.com
+// qumuqethu47@gmail.com
+// collinsb335@gmail.com
+// akpedefestus@gmail.com
+// xcryptonaire@gmail.com
+// theadabillions@gmail.com
+// josephjoey289@gmail.com
+// omachefujonah@yahoo.com
+// chuko1779@gmail.com
+// nwachukwucollins324@gmail.com
+// conquerbillions@gmail.com
+// mpozam79@gmail.com
+// skyllink@yahoo.com
+// onyedikafrank1@gmail.com
+// talabitope7@gmail.com
+// osinachiobi95@gmail.com
+// lovethizekor123@gmail.com
+// supet3017@gmail.com
+// samologunja2@gmail.com
+// 2023ola4life@gmail.com
+// abdul.tawfik25@gmail.com
+// udavid802@gmail.com
+// opeyeminelson2@gmail.com
+// ugokelvinoffor962@gmail.com
+// ndiokweluifeanyi64@gmail.com
+// abiodunmunna@gmail.com
+// jjjjrock9999@gmail.com
+// omoboyefemibamidele@gmail.com
+// salma.kimisha@gmail.com
+// umohp63@gmail.com
+// Emeriejchuks@gmail.com
+// andrewochigbu@gmail.com
+// Testeremail219@gmail.com
+// cryptoboomers7@gmail.com
+// sundaynwodo80s@gmail.com
+// squallemilz0@gmail.com
+// udikechisom@gmail.com
+// robinsonokoye2@gmail.com
+// kmartmicheals@gmail.com
+// festusigbolanu@gmail.com
+// greatworldng@gmail.com
+// bogdaniosifescu@yahoo.com
+// udekwuchukwuebuka@gmail.com
+// chukwuglory606@gmail.com
+// kufrejimmy32@gmail.com
+// ephraimodinaka22@gmail.com
+// shittusul20@gmail.com
+// agborzema7@gmail.com
+// fombangnelson908@gmail.com
+// gracefuambeh@gmail.com
+// adeyemi8160@gmail.com
+// eloskidyace@gmail.com
+// Timiadekunle2794@gmail.com
+// etwetebb@yahoo.com
+// janetoyelade@gmail.com
+// iwunzechidimma@gmail.com
+// iamprincechukwu@gmail.com
+// Kennedyalaka8@gmail.com
+// laryll4sure@gmail.com
+// bomageorge28@gmail.com
+// igbonanbuchi@gmail.com
+// seniosibanjo27@gmail.com
+// ajvictooor@gmail.com
+// mathydawu@gmail.com
+// akaron404@gmail.com
+// amezhinimwalele@gmail.com
+// amosoluwakorede@gmail.com
+// betterways247@gmail.com
+// temmysanusii@gmail.com
+// getapay@gmail.com
+// olatundejoshua93@gmail.com
+// amaobiisaiah@gmail.com
+// akum.njeri@gmail.com
+// agbohaprecious@gmail.com
+// brymefx@gmail.com
+// mmaeyen4christ2018@gmail.com
+// anjanbarai@gmail.com
+// billiom354@gmail.com
+// okonkwochiedoziejoseph@gmail.com
+// ukorahprince207@gmail.com
+// emekaibemene@gmail.com
+// odiraogbu@gmail.com
+// abdulwasiu.alli@gmail.com
+// olagunju6108@gmail.com
+// emmanuelik15@gmail.com
+// goodymomah8@gmail.com
+// sadeniyi009@gmail.com
+// apologunroy@gmail.com
+// hannahotuoba1984@gmail.com
+// uchendurosemary01@gmail.com
+// adenijigbenga9@gmail.com
+// joshuaapekey17@gmail.com
+// olafemi69@yahoo.com
+// gbengafalola445@gmail.com
+// hlalymokobocho@gmail.com
+// ibehrichard01@gmail.com
+// toluwalashe1995@gmail.com
+// Zonabitesilva@gmail.com
+// valentineoje@gmail.com
+// anuchambaw098@gmail.com
+// fastolpen@gmail.com
+// emmaigwe102@gmail.com
+// francisaniugwu@gmail.com
+// hurptimistic@gmail.com
+// onovocharles234@gmail.com
+// sholex57@gmail.com
+// uebube61@gmail.com
+// piusmathew1@gmail.com
+// largerben@gmail.com
+// oluwatomexy1993@gmail.com
+// sabiumeh@yahoo.com
+// emmanuelnnamdichukwu1996@gmail.com
+// elijahoreoluwa@gmail.com
+// ayodejiemmanuel301@gmail.com
+// muhisahabu70@gmail.com
+// kingsleyjacksonjolly@gmail.com
+// olufemiomiwole@gmail.com
+// deprince87v@gmail.com
+// goalachiever.inc@gmail.com
+// billionaireoma@outlook.com
+// yusufabdulsobur@gmail.com
+// livinusuchennaokafor0@gmail.com
+// eveora231@gmail.com
+// evelynbhaire@gmail.com
+// micahoffiong5@gmail.com
+// emmanuelagboha93@gmail.com
+// chidyke123@gmail.com
+// olisamex@yahoo.com
+// debrightdesk@gmail.com
+// bankyrhode@gmail.com
+// jsynajie06@gmail.com
+// ridwan8190@gmail.com
+// adeshinajeremiah66@gmail.com
+// steve.walker461@gmail.com
+// droppy53@gmail.com
+// dibady_jessica@yahoo.fr
+// mehtab007kn@gmail.com
+// wleo37@gmail.com
+// ayodelight45@gmail.com
+// jamestech128@gmail.com
+// dannydtrumpeter@gmail.com
+// joyce.ije62@gmail.com
+// abiodunyekeen76@gmail.com
+// vsmcrypto@gmail.com
+// sommy0748@gmail.com
+// umarfarouq202002@gmail.com
+// chiedochieo@gmail.com
+// chidiebereemmanuel1998@gmail.com
+// onorhovwa28@gmail.com
+// omokhagborphilip@gmail.com
+// xyznig@gmail.com
+// sheriffkunlex@gmail.com
+// anthonynwaekpe@yahoo.com
+// hackerrace393@gmail.com
+// rutafeovie19@gmail.com
+// obizallied@gmail.com
+// mightytechconcepts@gmail.com
+// shunamentown@gmail.com
+// sirmusteecrypto@gmail.com
+// solomonbreezy41@gmail.com
+// Chineduekeruche112@gmail.com
+// sholamcalex@gmail.com
+// angellaowukori@gmail.com
+// Ceesealtd@gmail.com
+// bunmifasan17@gmail.com
+// godonudaniel@gmail.com
+// felixobieje@gmail.com
+// cchummybest@gmail.com
+// emmyval90@gmail.com
+// kenisoko113@gmail.com
+// mebeisamuel@gmail.com
+// cryptos975@gmail.com
+// ogechukwumiriam@gmail.com
+// arinadeayo27@gmail.com
+// rickykumar2o2.a@gmail.com
+// olusolaadeaga@gmail.com
+// bownnikky@gmail.com
+// divinecarepharma@gmail.com
+// obierifrank@gmail.com
+// nzekweosondu@gmail.com
+// u.mayor1414@gmail.com
+// olusolalazarus@gmail.com
+// rapostlesam@gmail.com
+// Nnamdiojeh8@gmail.com
+// jrweb15@gmail.com
+// anyajeffezera@gmail.com
+// awhinemmanuel1@gmail.com
+// johncourtesy@gmail.com
+// lakucoin@gmail.com
+// josephsoonjin@gmail.com
+// ozodigboisaiah@gmail.com
+// pdsco2@gmail.com
+// teecogroup18@gmail.com
+// theocracy2011@yahoo.com
+// johnitoanito336@gmail.com
+// victoroliverotobo4@gmail.com
+// godwinmatthew90@gmail.com
+// qwintemmy1621@gmail.com
+// davidrichards1325@gmail.com
+// soalaharriet@gmail.com
+// mostvictor1@gmail.com
+// omanyalahenvick@gmail.com
+// johnsonighodo@gmail.com
+// ogelinks66@gmail.com
+// ucisblessed@gmail.com
+// emekamadu2@gmail.com
+// digitalprincess7@gmail.com
+// popsincj@gmail.com
+// isaacolatunbosun92@gmail.com
+// pault8054@gmail.com
+// nwajichristian10@gmail.com
+// umudeegs@gmail.com
+// excellit2020@gmail.com
+// nnyambura5@gmail.com
+// emmasalako@gmail.com
+// iykejay666@gmail.com
+// sololizzy19@gmail.com
+// bashdmarine@gmail.com
+// kharency48@gmail.com
+// oyeyemmyabdul@gmail.com
+// ojimaenemaku1@gmail.com
+// oyerindekemi2883@gmail.com
+// adabright77@gmail.com
+// Thomasoshioke59@gmail.com
+// andychinzy@gmail.com
+// niallhughes08@yahoo.com
+// moscolily@gmail.com
+// leonardibong@gmail.com
+// webb.3city@gmail.com
+// faithzakari24@gmail.com
+// olushalom1@gmail.com
+// www.professorsammy@gmail.com
+// wereypillz@gmail.com
+// olanipekunkayode4@gmail.com
+// kayodealoba2@gmail.com
+// johnsondodo208@gmail.com
+// rhotorufuos@gmail.com
+// tochienoch@gmail.com
+// swissebiefie28@gmail.com
+// ayenipriesthood@gmail.com
+// playstationpslg2425@gmail
+// Folajimisquare30@gmail.com
+// abubakaribrahim723@yahoo.com
+// emmatomjack@gmail.com
+// jamesajikson07@gmail.com
+// vwokekesiena@gmail.com
+// adelaide2580@gmail.com
+// anitaduadozdiary@gmail.com
+// Abdullahiisa226@gmail.com
+// tessyjayy@gmail.com
+// kellswealth@gmail.com
+// nathaniellisa9@gmail.com
+// ajogbejienterprises@gmail.com
+// mustapharabiu60@gmail.com
+// emmydia141@gmail.com
+// adeoluwatosin2575@gmail.com
+// inyamajimmy@gmail.com
+// kamnochsolutions@gmail.com
+// everfunfoods@gmail.com
+// itzbullet101@gmail.com
+// uche.ighavini@gmail.com
+// ifeikomi8@gmail.com
+// arsenkolly@gmail.com
+// ade.taofeeqhsc@gmail.com
+// edewornathan1999@gmail.com
+// swaggermipo@gmail.com
+// adexyuzzi08@gmail.com
+// adekunle.david1981@gmail.com
+// abbaswornowizzy@gmail.com
+// chukwumafrancis2004@gmail.com
+// martinsnwabueze005@gmail.com
+// jamesidun40@gmail.com
+// cogbonna582@gmail.com
+// ayonimofesuluka@gmail.com
+// jerrymyguy5@gmail.com
+// charlseedem2017@gmail.com
+// phindilejonas.pj@gmail.com
+// greatzele@gmail.com
+// omisa081@gmail.com
+// owoadehenry@gmail.com
+// siidneyacademy@gmail.com
+// joelkind2@gmail.com
+// dikaasimydikason@gmail.com
+// odumosuolashile@gmail.com
+// osundepat2020@gmail.com
+// cushlex3@gmail.com
+// michaelayodele0302@gmail.com
+// uzomimi42@gmail.com
+// blessedoti69@gmail.com
+// fasolaola007@gmail.com
+// chissomebube@gmail.com
+// omoaluseohue@gmail.com
+// edilovefx445@gmail.com
+// muazukuu@gmail.com
+// eketrustg8@gmail.com
+// endi4luv@gmail.com
+// okpara.ikemba@gmail.com
+// favour.somto@yahoo.com
+// abdulaicedamar69@gmail.com
+// emmyinvest29@gmail.com
+// udezestephen012@gmail.com
+// sylvester.sscc@gmail.com
+// norvera.world@gmail.com
+// alfbengreat@gmail.com
+// louisgodwin10@gmail.com
+// dicksonufot1997@gmail.com
+// ajibolaotesile@gmail.com
+// Lantamiracle@gmail.com
+// saminuusman857@gmail.com
+// belloadewale058@gmail.com
+// ijeomapatricia064@gmail.com
+// drealtimileyin@gmail.com
+// ndubuisikingsley379@gmail.com.xn--8ca
+// ebukarichies4@gmail.com
+// kudoncy4real@gmail.com
+// jain.reigns27@gmail.com
+// Chidijoshua20@gmail.com
+// fritzbernks@yahoo.com
+// stilljay24@outlook.com
+// cosmoselijah@gmail.com
+// abiola2lead@gmail.com
+// modisechoma@gmail.com
+// nurudeenafeez03@gmail.com
+// abdallahabdulwadud34@gmail.com
+// cuthbert2701@gmail.com
+// thobes75@gmail.com
+// nomehvictor1@gmail.com
+// dotun61@gmail.com
+// danrasco650@gmail.com
+// dmmachukwu@gmail.com
+// royce4you@yahoo.com
+// destinymerlin@yahoo.com
+// hirekson@gmail.com
+// mediositconsult@gmail.com
+// koses1107@gmail.com
+// sam66789708@gmail.com
+// akinduyomichael@gmail.com
+// elviseojong@gmail.com
+// dun97g@gmail.com
+// samuelgeo18k@gmail.com
+// holisshellon29@gmail.com
+// amusonick7@gmail.com
+// mbarichard18@gmail.com
+// blackpaintsil@gmail.com
+// lakshmanvepuri@gmail.com
+// chimonchimonkc@gmail.com
+// shore1luther@gmail.com
+// magnifico4804@gmail.com
+// Alabifaruk4@gmail.com
+// ibuklous@gmail.com
+// cc423551@gmail.com
+// vivicandy8@gmail.com
+// sobanjod@gmail.com
+// brpmicheal@gmail.com
+// madighinelson4@gmail.com
+// nsikakeffiong639@gmail.com
+// victory2013dxb@gmail.com
+// jollofboygram@gmail.com
+// 48hrbucks@gmail.com
+// barnaz843@gmail.com
+// najrahn2000@gmail.com
+// naijalag1@gmail.com
+// moyonom02@gmail.com
+// johngreatogar001@gmail.com
+// hussainiyakubu41@gmail.com
+// neonlounge59@gmail.com
---
+++ private-claim/src/app/page.tsx
@@ -0,0 +1,75 @@
+"use client";
+import { useState } from "react";
+import Image from "next/image";
+import { batch1, batch2 } from './data/data.js';
+import Link from "next/link";
+
+export default function Home() {
+  const currentYear = new Date().getFullYear();
+  const [email, setEmail] = useState('');
+  const [batch, setBatch] = useState('');
+  const [error, setError] = useState('');
+  const [link,setLink] = useState('')
+  const [pop , setPop] = useState(Boolean)
+
+  const checkBatch = () => {
+    setError('');
+    
+    // Corrected data usage
+    if (batch1.includes(email)) {
+      setBatch('Your part of Batch 1');
+      setLink('https://app.streamflow.finance/airdrop/solana/mainnet/H7FpvyY6eQTMfKcH8CYV2e9ofUPqdQ1dpoVaSAaBt9CT')
+      setPop(true)
+    } else if (batch2.includes(email)) {
+      setBatch(' Your part of Batch 2 ');
+      setLink('https://app.streamflow.finance/airdrop/solana/mainnet/DQn4W3UA52z2ZMGDmkaBpUv1QXpgv1DbQdzngZVM9MXV')
+      setPop(true)
+    } else {
+      setError('Email not found in either batch.');
+      setBatch('Your Batch is Not found')
+      setLink('No link for this Round')
+      setPop(false)
+
+    }
+  };
+  return (
+    <main className="flex items-center justify-center h-[100vh] flex-col">
+      <header className=" border border-b-[#111] absolute top-0 w-full items-center flex px-[10px] py-[20px] justify-center">
+      <Image
+                                                src="/Logo2.png"
+                                                alt="logo"
+                                                width={252}
+                                                height={252}
+                                                className=""
+                                            />
+      </header>
+
+
+      <div className=" max-w-[400px] w-full item-center justify-center  flex flex-col  ">
+        <div className="bg-[#1111] p-[20px] rounded-xl ">
+        <div className="text-[24px] font-bold">Confrim Batch to claim</div>
+        <input type="email" className=" my-[15px] h-[45px] w-full rounded-lg px-[10px] " placeholder="Enter Email here" value={email}
+        onChange={(e) => setEmail(e.target.value)}/>
+        <div className=" bg-[#008080] py-[10px] cursor-pointer flex items-center justify-center rounded-lg text-[#fff] hover:opacity-85" onClick={checkBatch}>Submit</div>
+
+        </div>
+      
+
+      </div>
+      { pop && 
+      <div className="max-w-[400px] w-full h-[150px] rounded-xl bg-[#1111] my-[50px] p-[20px] flex-wrap">
+        <h1 className="text-[24px] font-bold"> {batch}</h1>
+        <p className="mb-[15px]">Use this link to Claim 👇🏽 </p>
+        <Link href={link} className="w-auto h-[35px] rounded-[6px] bg-[#fff] text-[#851b1b]  p-[10px]">https://app.streamflow.finance/airdrop/sol...</Link>
+      </div>
+      }
+      
+      {error && <p className="py-[20px]" style={{ color: 'red' }}>🚫 {error}</p>}
+      <footer className="absolute bottom-0 py-[20px]">
+      <p>Copyright © {currentYear} DigiCask.Finance All rights reserved.</p>
+    </footer>
+
+        
+    </main>
+  );
+}
